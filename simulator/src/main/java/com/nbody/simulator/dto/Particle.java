d208d30c1aa34622d94548a4e83dd4ce99c8c04b
Tyler Russell <tyler.russell1@aol.com>
2024-08-21 19:33:02 -0500

    Adding getter/setter methods.

--- simulator/src/main/java/com/nbody/simulator/dto/Particle.java
+++ simulator/src/main/java/com/nbody/simulator/dto/Particle.java
@@ -6,4 +6,55 @@ public class Particle {
     Float vx;
     Float vy;
     Float m;
+
+    public Float getX() {
+        return x;
+    }
+
+    public void setX(Float x) {
+        this.x = x;
+    }
+
+    public Float getY() {
+        return y;
+    }
+
+    public void setY(Float y) {
+        this.y = y;
+    }
+
+    public Float getVx() {
+        return vx;
+    }
+
+    public void setVx(Float vx) {
+        this.vx = vx;
+    }
+
+    public Float getVy() {
+        return vy;
+    }
+
+    public void setVy(Float vy) {
+        this.vy = vy;
+    }
+
+    public Float getM() {
+        return m;
+    }
+
+    public void setM(Float m) {
+        this.m = m;
+    }
+
+    @Override
+    public String toString() {
+        return "Particle{" +
+                "x=" + x +
+                ", y=" + y +
+                ", vx=" + vx +
+                ", vy=" + vy +
+                ", m=" + m +
+                '}';
+    }
 }
