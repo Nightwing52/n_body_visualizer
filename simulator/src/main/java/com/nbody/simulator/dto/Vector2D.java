129a3a878bfebfb02b3d7d0272d072763703a2a6
Tyler Russell <tyler.russell1@aol.com>
2024-08-22 20:30:29 -0500

    Added Vector2D class.

---
+++ simulator/src/main/java/com/nbody/simulator/dto/Vector2D.java
@@ -0,0 +1,35 @@
+package com.nbody.simulator.dto;
+
+public class Vector2D {
+    float x;
+    float y;
+
+    public Vector2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector2D{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
