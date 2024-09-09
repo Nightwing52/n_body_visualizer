5d4815f415666677f96cfaa571d741d70f3f80d3
Tyler Russell <tyler.russelltx52@gmail.com>
2024-09-09 15:21:34 -0500

    Using SimulationService.

--- simulator/src/main/java/com/nbody/simulator/controller/SimulatorController.java
+++ simulator/src/main/java/com/nbody/simulator/controller/SimulatorController.java
@@ -1,52 +1,26 @@
 package com.nbody.simulator.controller;
 
-import com.nbody.simulator.dto.Frame;
-import com.nbody.simulator.dto.Particle;
 import com.nbody.simulator.dto.SimulationOutput;
 import com.nbody.simulator.dto.SimulationRequest;
+import com.nbody.simulator.service.SimulationService;
 import com.nbody.simulator.validator.SimulationRequestValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping(value = "/simulator")
 public class SimulatorController {
+    @Autowired
+    SimulationService simulationService;
+
     @PostMapping(value = "/v1/simulate")
     public ResponseEntity<SimulationOutput> simulate(@RequestBody SimulationRequest request) {
         // reflect input for now
         SimulationRequestValidator.validateSimulationRequest(request);
-        return ResponseEntity.ok(reflectInput(request));
-    }
-
-    private SimulationOutput reflectInput(SimulationRequest request) {
-        SimulationOutput simulationOutput = new SimulationOutput();
-        List<Frame> frameList = new ArrayList<>();
-        Frame frame = new Frame();
-        frame.setFrameNumber(0);
-
-        // getting particles
-        List<Particle> particleList = new ArrayList<>();
-        int N = request.getxList().size();
-        for(int i=0; i<N; i++) {
-            Particle newParticle = new Particle();
-            newParticle.setM(request.getmList().get(i));
-            newParticle.setX(request.getxList().get(i));
-            newParticle.setY(request.getyList().get(i));
-            newParticle.setVx(request.getVxList().get(i));
-            newParticle.setVy(request.getVyList().get(i));
-            particleList.add(newParticle);
-        }
-
-        frame.setParticleList(particleList);
-        frameList.add(frame);
-        simulationOutput.setFrameList(frameList);
-
-        return simulationOutput;
+        return ResponseEntity.ok(simulationService.simulate(request));
     }
 }
