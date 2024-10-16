520ef1188f615dce2513c106c0014cc759b03089
Tyler Russell <tyler.russelltx52@gmail.com>
2024-10-16 18:02:57 -0500

    Adding context loading test.

---
+++ simulator/src/test/java/com/nbody/simulator/MySpringBootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nbody.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Test if the Spring Boot context loads properly
+    }
+}
+
